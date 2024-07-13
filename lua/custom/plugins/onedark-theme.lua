b0fdf6fb9267a58c3e8a032f74a3b9237011b691
Micah Effiong <micaiah.effiong@gmail.com>
2024-07-13 22:36:32 +0100

    use opts for simple setups

--- lua/custom/plugins/onedark-theme.lua
+++ lua/custom/plugins/onedark-theme.lua
@@ -1,13 +1,13 @@
 return {
 	-- Theme inspired by Atom
 	'navarasu/onedark.nvim',
-	config = function()
-		require('onedark').setup {
-			priority = 1000,
-			toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
-			style = 'dark',
-			toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-		}
+	opts = {
+		priority = 1000,
+		toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
+		style = 'dark',
+		toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+	},
+	init = function()
 		vim.cmd.colorscheme 'onedark'
-	end,
+	end
 }
