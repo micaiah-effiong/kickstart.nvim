c93f9f85df128d356abd532c12110948e25a2ded
Micah Effiong <micaiah.effiong@gmail.com>
2024-08-29 00:26:09 +0100

    update and fix which-key errors

--- init.lua
+++ init.lua
@@ -155,22 +155,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
+require('which-key').add({
+  { '<leader>c', name = '[C]ode' },
+  { '<leader>d', name = '[D]ocument', },
+  { '<leader>g', name = '[G]it', },
+  { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+  { '<leader>r', name = '[R]ename', },
+  { '<leader>s', name = '[S]earch', },
+  { '<leader>t', name = '[T]oggle', },
+  { '<leader>w', name = '[W]orkspace', },
+
+  -- register which-key VISUAL mode
+  -- required for visual <leader>hs (hunk stage) to work
+  -- { '<leader>',  group = 'VISUAL <leader>', mode = { 'v' } },
+})
 
 -- custom settings
 pcall(require('custom.settings'))
