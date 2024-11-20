c19e194845cc091c34e17518756cec318bf65425
Micah Effiong <micaiah.effiong@gmail.com>
2024-11-20 09:03:40 +0100

    use onedark warmer theme style

--- lua/custom/plugins/onedark-theme.lua
+++ lua/custom/plugins/onedark-theme.lua
@@ -4,7 +4,7 @@ return {
 	opts = {
 		priority = 1000,
 		toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
-		style = 'dark',
+		style = 'warmer',
 		toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
 	},
 	init = function()
