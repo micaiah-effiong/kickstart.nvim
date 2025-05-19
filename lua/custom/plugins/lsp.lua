d0a0ae05f89219c2b3016b2bf591db595bd31d4b
Micah Effiong <micaiah.effiong@gmail.com>
2025-05-19 18:51:06 +0100

    code formating

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -42,12 +42,12 @@ return {
 	'neovim/nvim-lspconfig',
 	dependencies = {
 		-- Automatically install LSPs to stdpath for neovim
-		{ 'williamboman/mason.nvim', config = true },
-		{	'williamboman/mason-lspconfig.nvim', version = "v1.32.0",},
+		{ 'williamboman/mason.nvim',           config = true },
+		{ 'williamboman/mason-lspconfig.nvim', version = "v1.32.0", },
 
 		-- Useful status updates for LSP
 		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-		{ 'j-hui/fidget.nvim',       opts = {} },
+		{ 'j-hui/fidget.nvim',                 opts = {} },
 
 		-- Additional lua configuration, makes nvim stuff amazing!
 		'folke/neodev.nvim',
